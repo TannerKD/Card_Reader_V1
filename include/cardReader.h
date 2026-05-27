af3bd1b570206b16fedcd11d0669d431a4e7d2e1
tanner_d <tanner.dalde@gmail.com>
2026-05-27 17:20:14 +0200

    Finished implementation of cardReader class. Began implementation of member functions.

--- include/cardReader.h
+++ include/cardReader.h
@@ -1,6 +1,24 @@
 #ifndef CARDREADER_H
 #define CARDREADER_H
 
+#include <Adafruit_PN532.h>
+#include "Wire.h"
+#include "pins.h"
+
+class cardReader {
+public:
+    void setup();
+    void readDataBlock();
+
+private:
+    TwoWire PN532_I2C = TwoWire(1); // For hardware wire setup
+    Adafruit_PN532 *nfc; // creates PN532 object
+    uint8_t uid[6] = {0, 0, 0, 0, 0, 0}; // UID of NFC Tag
+    uint8_t uidLength; // Length of UID of NFC Tag
+    uint8_t blockData[16]; // Used for extracting block data, which is 16 bytes long
+    uint8_t keyNum = 0; // Key number = 0: key A, Key number = 1: key B
+    uint8_t keyA[6] = {0xff, 0xff, 0xff, 0xff, 0xff, 0xff}; // Basic key A for access to card
+};
 
 
 #endif
