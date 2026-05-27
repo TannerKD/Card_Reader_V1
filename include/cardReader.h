c97fcb87d2b6bfeaf875f8d8b038229a0a132a00
tanner_d <tanner.dalde@gmail.com>
2026-05-27 17:49:09 +0200

    Finished readCardData member function and successfully built the sketch.

--- include/cardReader.h
+++ include/cardReader.h
@@ -9,7 +9,7 @@ class cardReader {
 public:
     void setup();
     // reads data from block into blockData member variable. Possible block numbers are from 0-63.
-    void readDataBlock(uint8_t blockNumber);
+    void readCardData();
 
 private:
     TwoWire PN532_I2C = TwoWire(1); // For hardware wire setup
@@ -17,6 +17,7 @@ private:
     uint8_t uid[6] = {0, 0, 0, 0, 0, 0}; // UID of NFC Tag
     uint8_t uidLength; // Length of UID of NFC Tag
     uint8_t blockData[16]; // Used for extracting block data, which is 16 bytes long
+    uint8_t cardData[47][16];
     uint8_t keyNum = 0; // Key number = 0: key A, Key number = 1: key B
     uint8_t keyA[6] = {0xff, 0xff, 0xff, 0xff, 0xff, 0xff}; // Basic key A for access to card
 };
--- src/cardReader/cardReader.cpp
+++ src/cardReader/cardReader.cpp
@@ -12,9 +12,19 @@ void cardReader::setup() {
     // Implement LCD output for chip found
 }
 
-void cardReader::readDataBlock(uint8_t blockNumber) {
-    bool success; // Used for error handling
-    if (((nfc->mifareclassic_IsTrailerBlock(blockNumber)) == 0) && (blockNumber != 0)) {
-        success = nfc->mifareclassic_AuthenticateBlock(uid, uidLength, blockNumber, keyNum, keyA);
+void cardReader::readCardData() {
+    // Reads all blocks from 0 to 63. Will not read first block (1 total) or section trailers (16 total). Total blocks read is 47.
+    for (int currentBlock = 0; currentBlock < 64; currentBlock++)
+    if (((nfc->mifareclassic_IsTrailerBlock(currentBlock)) == 0) && (currentBlock != 0)) {
+        if (nfc->mifareclassic_AuthenticateBlock(uid, uidLength, currentBlock, keyNum, keyA)) {
+            if (nfc->mifareclassic_ReadDataBlock(currentBlock, blockData)) {
+                memcpy(cardData[currentBlock],blockData,16);
+            } else {
+                // Implement LCD output for readblock error
+            }
+        } else {
+            // Implement LCD output for authentication error
+        }
     }
+
 }
