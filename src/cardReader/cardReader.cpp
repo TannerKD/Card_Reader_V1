a00c57d47831c93479706f0e84cc380b39cbddce
tanner_d <tanner.dalde@gmail.com>
2026-05-27 17:23:03 +0200

    Finished cardReader setup function.

--- src/cardReader/cardReader.cpp
+++ src/cardReader/cardReader.cpp
@@ -4,5 +4,10 @@ void cardReader::setup() {
     // Set up hardware connections
     PN532_I2C.begin(Pins::PN532_SDA, Pins::PN532_SCL);
     nfc = new Adafruit_PN532(Pins::PN532_IRQ, Pins::PN532_RST, &PN532_I2C);
-
+    uint32_t versionData = nfc->getFirmwareVersion();
+    if (!versionData) {
+        // Implement LCD output for error
+        while (1); // halt
+    }
+    // Implement LCD output for chip found
 }
