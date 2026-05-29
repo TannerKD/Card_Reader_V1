37d63f698bcdc9a706fab28c884c0506e91fe2f0
tanner_d <tanner.dalde@gmail.com>
2026-05-29 08:39:33 +0200

    Fixed all setup code within void loop.

--- src/main.cpp
+++ src/main.cpp
@@ -7,21 +7,23 @@ mqttClient client;
 cardReader nfcTagReader;
 
 void setup() {
+    // Serial
+    Serial.begin(9600);
+    
+    // Queue Initialization
+
+    // Setup Functions
     pin_setup();
     nfcTagReader.setup();
     client.mqttClient::wifiSetup();
     client.mqttClient::mqttSetup();
-}
-
-void loop() {
-    // Serial
-    Serial.begin(9600);
-    // Queue Initialization
-
-    // Setup Functions
 
     // RTOS Task Creation
     xTaskCreate(tPulseLED, "Pulse LED", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
     xTaskCreate(tCardRead, "Card Read", 4096, NULL, 2, NULL);
     xTaskCreate(tMQTTPubSub, "MQTT Publisher/Subscriber", 4096, NULL, 2, NULL);
+}
+
+void loop() {
+
 }
